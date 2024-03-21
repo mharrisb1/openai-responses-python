26be3d71617a77b294377a91cd01c1f8563755e6
Michael Harris <mharris@luabase.com>
2024-03-20 20:05:42 -0400

    feat(pytest): add plugin setup

---
+++ src/openai_responses/plugin.py
@@ -0,0 +1,83 @@
+import pytest
+
+from openai_responses import (
+    AssistantsMock,
+    ChatCompletionMock,
+    EmbeddingsMock,
+    FilesMock,
+    ThreadsMock,
+    MessagesMock,
+    RunsMock,
+)
+
+
+def pytest_configure(config: pytest.Config):
+    config.addinivalue_line(
+        "markers",
+        "assistants_mock: OpenAI assistants API mocker",
+    )
+
+    config.addinivalue_line(
+        "markers",
+        "chat_completion_mock: OpenAI chat completion API mocker",
+    )
+
+    config.addinivalue_line(
+        "markers",
+        "embeddings_mock: OpenAI embeddings API mocker",
+    )
+
+    config.addinivalue_line(
+        "markers",
+        "files_mock: OpenAI files API mocker",
+    )
+
+    config.addinivalue_line(
+        "markers",
+        "threads_mock: OpenAI threads API mocker",
+    )
+
+    config.addinivalue_line(
+        "markers",
+        "messages_mock: OpenAI messages API mocker",
+    )
+
+    config.addinivalue_line(
+        "markers",
+        "runs_mock: OpenAI runs API mocker",
+    )
+
+
+@pytest.fixture()
+def assistants_mock() -> AssistantsMock:
+    return AssistantsMock()
+
+
+@pytest.fixture()
+def chat_completion_mock() -> ChatCompletionMock:
+    return ChatCompletionMock()
+
+
+@pytest.fixture()
+def embeddings_mock() -> EmbeddingsMock:
+    return EmbeddingsMock()
+
+
+@pytest.fixture()
+def files_mock() -> FilesMock:
+    return FilesMock()
+
+
+@pytest.fixture()
+def threads_mock() -> ThreadsMock:
+    return ThreadsMock()
+
+
+@pytest.fixture()
+def messages_mock() -> MessagesMock:
+    return MessagesMock()
+
+
+@pytest.fixture()
+def runs_mock() -> RunsMock:
+    return RunsMock()
