53c536c78d9594040b9b092159b4674eb4b04f11
Michael Harris <mharris@luabase.com>
2024-03-21 14:17:37 -0400

    chore(tests): test unwrap

--- tests/unit/test_decorators.py
+++ tests/unit/test_decorators.py
@@ -4,10 +4,10 @@ from typing import Any
 import httpx
 import respx
 
-from openai_responses.decorators import side_effect
+from openai_responses.decorators import side_effect, unwrap
 
 
-def test_side_effect_decorator():
+def test_side_effect_make_decorator():
     @side_effect
     def wrapped_side_effect(route: respx.Route, **kwargs: Any) -> httpx.Response:
         return httpx.Response(status_code=200)
@@ -23,3 +23,31 @@ def test_side_effect_decorator():
     t2 = time.time()
     assert res.status_code == 200
     assert (t2 - t1) > 1
+
+
+def test_unwrap():
+    global i
+    i = 0
+
+    def foo_decorator(func: Any):
+        def wrapper():
+            global i
+            i += 1
+            return func()
+
+        return wrapper
+
+    @foo_decorator
+    @foo_decorator
+    def my_function():
+        global i
+        i += 1
+        return i
+
+    assert my_function() == 3
+
+    i = 0
+
+    original = unwrap(my_function)
+
+    assert original() == 1
