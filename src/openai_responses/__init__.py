a681631c79fb2522c5cf1cb4f4bbebdd6d46105a
Michael Harris <mharris@luabase.com>
2024-03-20 20:06:36 -0400

    feat(core): add main api

---
+++ src/openai_responses/__init__.py
@@ -0,0 +1,35 @@
+from openai_responses.endpoints.assistants import AssistantsMock
+from openai_responses.endpoints.chat import ChatMock, ChatCompletionMock
+from openai_responses.endpoints.embeddings import EmbeddingsMock
+from openai_responses.endpoints.files import FilesMock
+from openai_responses.endpoints.threads import ThreadsMock, MessagesMock, RunsMock
+
+__all__ = [
+    # main API
+    "mock",
+    # mockers
+    "AssistantsMock",
+    "ChatCompletionMock",
+    "EmbeddingsMock",
+    "FilesMock",
+    "ThreadsMock",
+    "MessagesMock",
+    "RunsMock",
+]
+
+
+class Api:
+    def __init__(self) -> None:
+        self.files = FilesMock()
+        self.embeddings = EmbeddingsMock()
+        self.chat = ChatMock()
+        self.beta = Beta()
+
+
+class Beta:
+    def __init__(self) -> None:
+        self.assistants = AssistantsMock()
+        self.threads = ThreadsMock()
+
+
+mock = Api()
