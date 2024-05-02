c0c73eeaef120f415b84df030bdd03bc40f0babe
Michael Harris <mharris@luabase.com>
2024-05-02 12:37:43 -0400

    chore(api): move to init

--- src/openai_responses/__init__.py
+++ src/openai_responses/__init__.py
@@ -1,16 +1,19 @@
 from httpx import Request, Response
 from respx import Route
 
-from ._api import mock
 from ._mock import OpenAIMock
+from ._stores import StateStore
 
 __all__ = [
     # main interface
     "mock",
-    # internal classes for type annotations
+    # internal
     "OpenAIMock",
-    # forwarded external classes for type annotations
+    "StateStore",
+    # external
     "Request",
     "Response",
     "Route",
 ]
+
+mock = OpenAIMock()
--- src/openai_responses/_api.py
+++
@@ -1,3 +0,0 @@
-from ._mock import OpenAIMock
-
-mock = OpenAIMock()
