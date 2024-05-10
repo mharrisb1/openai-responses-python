48e1e6f74202b8e67ed779c999e970d006028cc5
Michael Harris <mharris@luabase.com>
2024-05-10 10:38:30 -0400

    chore(api): add docstring for args

--- src/openai_responses/_api.py
+++ src/openai_responses/_api.py
@@ -9,5 +9,10 @@ def mock(
     base_url: Optional[str] = None,
     state: Optional[StateStore] = None,
 ):
+    """
+    Args:
+        base_url (Optional[str], optional): Override base URL. Defaults to None.
+        state (Optional[StateStore], optional): Override default empty state. Defaults to None.
+    """
     openai_mock = OpenAIMock(base_url, state)
     return openai_mock._start_mock()
