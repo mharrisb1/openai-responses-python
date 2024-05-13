1c53959c539b09f351148d530fe4f9247b5343ca
Michael Harris <mharris@luabase.com>
2024-05-13 11:42:32 -0400

    feat(examples): add run step route examples

---
+++ examples/test_run_steps.py
@@ -0,0 +1,68 @@
+import openai
+
+import openai_responses
+from openai_responses import OpenAIMock
+from openai_responses.helpers.builders.messages import build_message
+from openai_responses.helpers.builders.run_steps import build_run_step
+from openai_responses.helpers.state_store import add_resource_to_state_store
+
+
+@openai_responses.mock()
+def test_list_run_steps(openai_mock: OpenAIMock):
+    client = openai.Client(api_key="sk-fake123")
+
+    assistant = client.beta.assistants.create(
+        instructions="You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
+        name="Math Tutor",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-4-turbo",
+    )
+
+    thread = client.beta.threads.create()
+
+    run = client.beta.threads.runs.create(
+        thread.id,
+        assistant_id=assistant.id,
+    )
+
+    # NOTE: need to manually construct assistant message and run step
+    assistant_message = build_message(
+        {
+            "assistant_id": assistant.id,
+            "content": [
+                {
+                    "type": "text",
+                    "text": {
+                        "annotations": [],
+                        "value": "Hello! Feel free to ask me any questions.",
+                    },
+                }
+            ],
+            "role": "assistant",
+            "run_id": run.id,
+            "status": "completed",
+            "thread_id": thread.id,
+        }
+    )
+
+    run_step = build_run_step(
+        {
+            "assistant_id": assistant.id,
+            "thread_id": thread.id,
+            "run_id": run.id,
+            "status": "in_progress",
+            "type": "message_creation",
+            "step_details": {
+                "type": "message_creation",
+                "message_creation": {
+                    "message_id": assistant_message.id,
+                },
+            },
+        }
+    )
+    add_resource_to_state_store(assistant_message, mock=openai_mock)
+    add_resource_to_state_store(run_step, mock=openai_mock)
+
+    steps = client.beta.threads.runs.steps.list(run.id, thread_id=thread.id)
+
+    assert len(steps.data) == 1
