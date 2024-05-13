7949be9fc2b6b91820a45f2805e2261dd7d0c6d4
Michael Harris <mharris@luabase.com>
2024-05-13 10:24:29 -0400

    feat(routes): add run step routes

---
+++ src/openai_responses/_types/partials/run_steps.py
@@ -0,0 +1,120 @@
+from typing import Annotated, Dict, List, Literal, TypedDict, Union
+from typing_extensions import NotRequired
+
+from openai._utils import PropertyInfo
+
+__all__ = ["PartialRunStep", "PartialRunStepList"]
+
+
+class PartialLastError(TypedDict):
+    code: Literal["server_error", "rate_limit_exceeded"]
+    message: str
+
+
+class PartialMessageCreation(TypedDict):
+    message_id: str
+
+
+class PartialMessageCreationStepDetails(TypedDict):
+    message_creation: PartialMessageCreation
+    type: Literal["message_creation"]
+
+
+class PartialCodeInterpreterOutputLogs(TypedDict):
+    logs: str
+    type: Literal["logs"]
+
+
+class PartialCodeInterpreterOutputImageImage(TypedDict):
+    file_id: str
+
+
+class PartialCodeInterpreterOutputImage(TypedDict):
+    image: PartialCodeInterpreterOutputImageImage
+    type: Literal["image"]
+
+
+class PartialCodeInterpreter(TypedDict):
+    input: str
+    outputs: List[
+        Annotated[
+            Union[PartialCodeInterpreterOutputLogs, PartialCodeInterpreterOutputImage],
+            PropertyInfo(discriminator="type"),
+        ]
+    ]
+
+
+class PartialCodeInterpreterToolCall(TypedDict):
+    id: str
+    code_interpreter: PartialCodeInterpreter
+    type: Literal["code_interpreter"]
+
+
+class PartialFileSearchToolCall(TypedDict):
+    id: str
+    file_search: object
+    type: Literal["file_search"]
+
+
+class PartialFunction(TypedDict):
+    arguments: str
+    name: str
+    output: NotRequired[str]
+
+
+class PartialFunctionToolCall(TypedDict):
+    id: str
+    function: PartialFunction
+    type: Literal["function"]
+
+
+class PartialToolCallsStepDetails(TypedDict):
+    tool_calls: Annotated[
+        Union[
+            PartialCodeInterpreterToolCall,
+            PartialFileSearchToolCall,
+            PartialFunctionToolCall,
+        ],
+        PropertyInfo(discriminator="type"),
+    ]
+    type: Literal["tool_calls"]
+
+
+class PartialUsage(TypedDict):
+    completion_tokens: int
+    prompt_tokens: int
+    total_tokens: int
+
+
+class PartialRunStep(TypedDict):
+    id: NotRequired[str]
+    assistant_id: NotRequired[str]
+    cancelled_at: NotRequired[int]
+    completed_at: NotRequired[int]
+    created_at: NotRequired[int]
+    expired_at: NotRequired[int]
+    failed_at: NotRequired[int]
+    last_error: NotRequired[PartialLastError]
+    metadata: NotRequired[Dict[str, str]]
+    object: Literal["thread.run.step"]
+    run_id: NotRequired[str]
+    status: NotRequired[
+        Literal["in_progress", "cancelled", "failed", "completed", "expired"]
+    ]
+    step_details: NotRequired[
+        Annotated[
+            Union[PartialMessageCreationStepDetails, PartialToolCallsStepDetails],
+            PropertyInfo(discriminator="type"),
+        ]
+    ]
+    thread_id: NotRequired[str]
+    type: NotRequired[Literal["message_creation", "tool_calls"]]
+    usage: NotRequired[PartialUsage]
+
+
+class PartialRunStepList(TypedDict):
+    object: NotRequired[Literal["list"]]
+    data: NotRequired[List[PartialRunStep]]
+    first_id: NotRequired[str]
+    last_id: NotRequired[str]
+    has_more: NotRequired[bool]
