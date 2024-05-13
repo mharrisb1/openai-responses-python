303d559094514adff786d840914377c083276a6a
Michael Harris <mharris@luabase.com>
2024-05-13 11:44:18 -0400

    feat(builders): add build message helper

--- src/openai_responses/helpers/builders/messages.py
+++ src/openai_responses/helpers/builders/messages.py
@@ -8,7 +8,11 @@ from ._base import _generic_builder
 from ..._routes.messages import MessageCreateRoute
 from ..._types.partials.messages import PartialMessage
 
-__all__ = ["message_from_create_request"]
+from ..._utils.faker import faker
+from ..._utils.serde import model_parse
+from ..._utils.time import utcnow_unix_timestamp_s
+
+__all__ = ["message_from_create_request", "build_message"]
 
 
 def message_from_create_request(
@@ -21,3 +25,12 @@ def message_from_create_request(
     if extra:
         partial |= extra
     return _generic_builder(MessageCreateRoute, request, partial)
+
+
+def build_message(partial: PartialMessage) -> Message:
+    default: PartialMessage = {
+        "id": faker.beta.thread.message.id(),
+        "created_at": utcnow_unix_timestamp_s(),
+        "object": "thread.message",
+    }
+    return model_parse(Message, default | partial)
