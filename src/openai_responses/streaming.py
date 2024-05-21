b28cb89af03f9c8a3707f1b27964fc4e1d40c371
Michael Harris <mharris@luabase.com>
2024-05-21 14:23:36 -0400

    feat(streaming): add event stream classes

---
+++ src/openai_responses/streaming.py
@@ -0,0 +1,171 @@
+import json
+from dataclasses import dataclass
+from typing import AsyncGenerator, Generator, Literal, Optional, Tuple, Union, overload
+
+from openai.types import ErrorObject
+from openai.types.chat.chat_completion_chunk import ChatCompletionChunk
+
+from openai.types.beta.thread import Thread
+from openai.types.beta.threads.message import Message
+from openai.types.beta.threads.message_delta import MessageDelta
+from openai.types.beta.threads.run import Run
+from openai.types.beta.threads.runs.run_step import RunStep
+from openai.types.beta.threads.runs.run_step_delta import RunStepDelta
+
+from ._utils.serde import model_dict
+
+__all__ = ["EventStream", "AsyncEventStream"]
+
+EventType = Literal[
+    "thread.created",
+    "thread.run.created",
+    "thread.run.queued",
+    "thread.run.in_progress",
+    "thread.run.requires_action",
+    "thread.run.completed",
+    "thread.run.incomplete",
+    "thread.run.failed",
+    "thread.run.cancelling",
+    "thread.run.cancelled",
+    "thread.run.expired",
+    "thread.run.step.created",
+    "thread.run.step.in_progress",
+    "thread.run.step.delta",
+    "thread.run.step.completed",
+    "thread.run.step.failed",
+    "thread.run.step.cancelled",
+    "thread.run.step.expired",
+    "thread.message.created",
+    "thread.message.in_progress",
+    "thread.message.delta",
+    "thread.message.completed",
+    "thread.message.incomplete",
+    "error",
+]
+EventData = Union[
+    ChatCompletionChunk,
+    Thread,
+    Run,
+    RunStep,
+    RunStepDelta,
+    Message,
+    MessageDelta,
+    ErrorObject,
+]
+
+
+@dataclass(frozen=True)
+class Event:
+    type: Optional[EventType]
+    data: EventData
+
+    def to_sse_event(self) -> Tuple[Optional[bytes], Optional[bytes]]:
+        encoded_event = f"event: {self.type}\n".encode() if self.type else None
+        encoded_data = f"data: {json.dumps(model_dict(self.data))}\n\n".encode()
+        return encoded_event, encoded_data
+
+
+class BaseEventStream:
+    @overload
+    def event(self, type: Literal["thread.created"], data: Thread) -> Event: ...
+
+    @overload
+    def event(
+        self,
+        type: Literal[
+            "thread.run.created",
+            "thread.run.queued",
+            "thread.run.in_progress",
+            "thread.run.requires_action",
+            "thread.run.completed",
+            "thread.run.incomplete",
+            "thread.run.failed",
+            "thread.run.cancelling",
+            "thread.run.cancelled",
+            "thread.run.expired",
+        ],
+        data: Run,
+    ) -> Event: ...
+
+    @overload
+    def event(
+        self,
+        type: Literal[
+            "thread.run.step.created",
+            "thread.run.step.in_progress",
+            "thread.run.step.completed",
+            "thread.run.step.failed",
+            "thread.run.step.cancelled",
+            "thread.run.step.expired",
+        ],
+        data: RunStep,
+    ) -> Event: ...
+
+    @overload
+    def event(
+        self, type: Literal["thread.run.step.delta"], data: RunStepDelta
+    ) -> Event: ...
+
+    @overload
+    def event(
+        self,
+        type: Literal[
+            "thread.message.created",
+            "thread.message.in_progress",
+            "thread.message.completed",
+            "thread.message.incomplete",
+        ],
+        data: Message,
+    ) -> Event: ...
+
+    @overload
+    def event(
+        self, type: Literal["thread.message.delta"], data: MessageDelta
+    ) -> Event: ...
+
+    @overload
+    def event(self, type: Literal["error"], data: ErrorObject) -> Event: ...
+
+    @overload
+    def event(self, type: None, data: ChatCompletionChunk) -> Event: ...
+
+    def event(self, type: Optional[EventType], data: EventData) -> Event:
+        """
+        Create a server sent event payload with optional event.type and event.data payloads
+        """
+        return Event(type, data)
+
+
+class EventStream(BaseEventStream):
+    """Event stream protocol for building mock OpenAI server sent event stream"""
+
+    def __iter__(self) -> Generator[bytes, None, None]:
+        for _event in self.generate():
+            t, d = _event.to_sse_event()
+            if t:
+                yield t
+            if d:
+                yield d
+
+        yield b"event: done\n"
+        yield b"data: [DONE]\n\n"
+
+    def generate(self) -> Generator[Event, None, None]:
+        raise NotImplementedError
+
+
+class AsyncEventStream(BaseEventStream):
+    """Async event stream protocol for building mock OpenAI server sent event stream"""
+
+    async def __aiter__(self) -> AsyncGenerator[bytes, None]:
+        async for _event in await self.generate():
+            t, d = _event.to_sse_event()
+            if t:
+                yield t
+            if d:
+                yield d
+        yield b"event: done\n"
+        yield b"data: [DONE]\n\n"
+
+    async def generate(self) -> AsyncGenerator[Event, None]:
+        raise NotImplementedError
