99a77d19cb2d23bc7c812233eaf796c14ebd2266
Michael Harris <mharris@definite.app>
2024-09-27 10:41:20 -0400

    chore(deps): hoist min version to v1.50 (#65)
    * fix(types): update moderation types
    
    * chore(deps): hoist minimum version to v1.50

--- src/openai_responses/_types/partials/moderation.py
+++ src/openai_responses/_types/partials/moderation.py
@@ -1,10 +1,11 @@
-from typing import List, TypedDict
+from typing import List, Literal, TypedDict
 from typing_extensions import NotRequired
 
 __all__ = [
     "PartialModerationCreateResponse",
     "PartialCategories",
     "PartialCategoryScores",
+    "PartialCategoryAppliedInputTypes",
 ]
 
 PartialCategories = TypedDict(
@@ -14,6 +15,8 @@ PartialCategories = TypedDict(
         "harassment/threatening": NotRequired[bool],
         "hate": NotRequired[bool],
         "hate/threatening": NotRequired[bool],
+        "illicit": NotRequired[bool],
+        "illicit/violent": NotRequired[bool],
         "self-harm": NotRequired[bool],
         "self-harm/instructions": NotRequired[bool],
         "self-harm/intent": NotRequired[bool],
@@ -31,6 +34,8 @@ PartialCategoryScores = TypedDict(
         "harassment/threatening": NotRequired[float],
         "hate": NotRequired[float],
         "hate/threatening": NotRequired[float],
+        "illicit": NotRequired[float],
+        "illicit/violent": NotRequired[float],
         "self-harm": NotRequired[float],
         "self-harm/instructions": NotRequired[float],
         "self-harm/intent": NotRequired[float],
@@ -38,6 +43,25 @@ PartialCategoryScores = TypedDict(
         "sexual/minors": NotRequired[float],
         "violence": NotRequired[float],
         "violence/graphic": NotRequired[float],
+    },
+)
+
+PartialCategoryAppliedInputTypes = TypedDict(
+    "PartialCategoryAppliedInputTypes",
+    {
+        "harassment": NotRequired[List[Literal["text"]]],
+        "harassment/threatening": NotRequired[List[Literal["text"]]],
+        "hate": NotRequired[List[Literal["text"]]],
+        "hate/threatening": NotRequired[List[Literal["text"]]],
+        "illicit": NotRequired[List[Literal["text"]]],
+        "illicit/violent": NotRequired[List[Literal["text"]]],
+        "self-harm": NotRequired[List[Literal["text", "image"]]],
+        "self-harm/instructions": NotRequired[List[Literal["text", "image"]]],
+        "self-harm/intent": NotRequired[List[Literal["text", "image"]]],
+        "sexual": NotRequired[List[Literal["text", "image"]]],
+        "sexual/minors": NotRequired[List[Literal["text"]]],
+        "violence": NotRequired[List[Literal["text", "image"]]],
+        "violence/graphic": NotRequired[List[Literal["text", "image"]]],
     },
 )
 
